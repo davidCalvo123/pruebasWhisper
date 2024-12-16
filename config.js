8ab291cf7acbb89f19d2e07051335de2d3620fe8
davidCalvo123 <deivirayo02@gmail.com>
2024-12-16 14:34:20 +0100

    configuracion de mysql

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ module.exports = {
       enabled: true,
       server: 'localhost', 
       port: 9001, 
-      apiKey: 'ef4f7a8e7898cecaa0038e03faf63e9f8422a51e799919fb7036046ef1d5fb90' 
+      apiKey: 'ef4f7a8e7898cecaa0038e03faf63e9f8422a51e799919fb7036046ef1d5fb90', 
+    },
+    database: {
+      host: 'localhost',
+      user: 'etherpad_user',
+      password: 'Etherpad!2024',
+      database: 'etherpad_db',
     }
   };
+  
