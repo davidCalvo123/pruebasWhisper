89063eba1d1f03f4e8ce6ca431f9a86158a63955
davidCalvo123 <deivirayo02@gmail.com>
2024-12-16 12:28:40 +0100

    installacion etherpad

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    etherpad: {
+      enabled: true,
+      server: 'localhost', 
+      port: 9001, 
+      apiKey: 'ef4f7a8e7898cecaa0038e03faf63e9f8422a51e799919fb7036046ef1d5fb90' 
+    }
+  };
